7657110d82853867201757aa7876e49f69c5bda1
UntuckedShirt <75454794+UntuckedShirt@users.noreply.github.com>
2021-11-12 16:11:32 -0500

    adding landscape to board

--- game.js
+++ game.js
@@ -6,10 +6,46 @@ kaboom({
     clearColor: [0, 0, 0, 1],
 })
 
+loadRoot('https://i.imgur.com/')
+loadSprite('coin', 'wbKxhcd.png')
+loadSprite('evil-goomba', 'KPO3fR9.png')
+loadSprite('brick', 'pogC9x5.png')
+loadSprite('block', 'M6rwarW.png')
+loadSprite('mario', 'Wb1qfhK.png')
+loadSprite('shroom', '0wMd92p.png')
+loadSprite('surprise-box', 'gesQ1KP.png')
+loadSprite('unboxed', 'bdrLpi6.png')
+loadSprite('pipe-top-left', 'ReTPiWY.png')
+loadSprite('pipe-top-right', 'hj2GK4n.png')
+loadSprite('pipe-bottom-left', 'c1cYSbt.png')
+loadSprite('pipe-bottom-right', 'nqQ79eI.png')
+
+
 scene("game", () => {
     layers('bg', 'obj', 'ui', 'obj')
 
-    
+    const map = [
+        '                                                    ',
+        '                                                    ',
+        '                                                    ',
+        '                                                    ',
+        '                                                    ',
+        '                                                    ',
+        '                                                    ',
+        '                                                    ',
+        '                                                    ',
+        '                                                    ',
+        '                                                    ',
+        
+        '================================  ================== ===========    =========='
+    ]
+    const levelCfg = {
+        width: 20,
+        height: 20,
+        '=': [sprite('block')]
+}
+
+
 })
 
 
